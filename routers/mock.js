fe6f445ee096c4c65e63c20b02e9687141afb017
小乌龟 <666666@qq.com>
2018-09-28 18:41:16 +0800

    自动更新

--- routers/mock.js
+++ routers/mock.js
@@ -7,7 +7,7 @@ router.get('/list', (req, res, next) => {
     const data = Mock.mock({
         'total': 100,
         // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
-        'list|100-200': [{
+        'list|100-2111': [{
             'id|+1': 1,
             'title': '@ctitle(15, 20)',
             'status': '@pick(["审核", "预申请", "转正", "正式", "已取消"])',
